57cd5aa578542811b5bc6271bf2a22812027c18d
arthur <arthur@darkstar.example.net>
2024-02-26 00:51:05 +1300

    Updated

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # MSX-SFG-01-YM2151
-VGM to ROM converter
+
+Convert a VGM YM2151 file into a ROM that can be used with a Yamaha CX5M MSX computer.
+
+Currently only parses some VGM files...
+
+## Legal
+
+Copyright (c) 2024 Arthur Choung. All rights reserved.
+
+Email: arthur -at- hotdoglinux.com
+
+Released under the GNU General Public License, version 3.
+
+For details on the license, refer to the LICENSE file.
+
--- make_example.sh
+++ make_example.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-set -x
 set -e
 
 ( cd external/z80asm-1.8 && make )
@@ -10,5 +9,10 @@ cat example_contra04.vgm | ./convert >data.asm
 
 external/z80asm-1.8/z80asm -o example.rom player.asm
 
-echo "Use the command: openmsx -machine Yamaha_CX5M -cart example.rom -romtype ascii8"
-
+echo
+echo "Done"
+echo
+echo "Use the command:"
+echo
+echo "openmsx -machine Yamaha_CX5M -cart example.rom -romtype ascii8"
+echo
